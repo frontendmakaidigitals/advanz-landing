5a042b5196cb64c8167f74700e79201442d65f16
Faheem <faheemk793@gmail.com>
2026-01-19 17:25:04 +0530

    added stat card

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import Hero from "./sections/hero";
 import Testimonial from "./components/Testimonial";
 import Services from "./sections/services";
 import CTA from "./sections/cta";
+import StatCard from "./components/stat-card";
 const page = () => {
   return (
     <main>
       <Hero />
       <Services />
+      <div className="max-w-6xl px-5 mx-auto">
+        <StatCard />
+      </div>
       <Testimonial />
       <CTA />
     </main>
